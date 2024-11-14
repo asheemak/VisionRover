b3b8e27282ae71c03938f4bad7e8eb076a3a7056
Manoochehr Mojgani <mmojgani77@gmail.com>
2024-11-14 14:10:25 +0330

    Update VisionRover.py

--- VisionRover.py
+++ VisionRover.py
@@ -1,3 +1,10 @@
-class Loaders:
-    def LoadImage(self):
-        print("Image Loaded!")
+import cv2
+
+def contourSolidity(contour):
+	
+	contour_area = cv2.contourArea(contour)  
+	convex_hull = cv2.convexHull(contour)  
+	convex_area = cv2.contourArea(convex_hull)  
+	
+	solidity = contour_area / convex_area if convex_area != 0 else 0
+	return solidity
