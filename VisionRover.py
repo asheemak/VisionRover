9fa8c6f217e4fd685984a31d96e28fa266aea117
Manoochehr Mojgani <mmojgani77@gmail.com>
2024-11-15 11:16:47 +0330

    Apply new methods to VisionRover.py

--- VisionRover.py
+++ VisionRover.py
@@ -8,6 +8,54 @@ def contourSolidity(contour):
 	solidity = contour_area / convex_area if convex_area != 0 else 0
 	return solidity
 
+
+def contourFactorStruct(contour):
+    contour_area = cv2.contourArea(contour)  
+    perimeter = cv2.arcLength(contour, True)  
+    structure = (perimeter ** 2) / contour_area if contour_area != 0 else 0
+    return structure
+
+
+def contourAnisometry(contour):
+    if len(contour) >= 5: 
+        ellipse = cv2.fitEllipse(contour)
+        MA, ma = ellipse[1]  
+        aspect_ratio = MA / ma if ma != 0 else 0
+        anisometry = aspect_ratio
+    else:
+        anisometry = 0
+
+    return anisometry
+
+
+
+def contourAxisMinorLength(contour):
+    if len(contour) >= 5:
+        ellipse = cv2.fitEllipse(contour)
+        _, (MA, ma), _ = ellipse
+        length = min(MA, ma)
+    else:
+        length = 0
+
+    return length
+
+
+def contourAxisMajorLength(contour):
+    if len(contour) >= 5:
+        ellipse = cv2.fitEllipse(contour)
+        _, (MA, ma), _ = ellipse
+        length = max(MA, ma)
+    else:
+        length = 0
+    
+    return length
+
+
+def contourOuterRadius(contour):
+    _, radius = cv2.minEnclosingCircle(contour)
+    return radius
+
+
 def contourBulkiness(contour):
     contour_area = cv2.contourArea(contour)  
     convex_hull = cv2.convexHull(contour)    
